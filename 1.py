d3639a63bab152df8d4ea35ebb66e91b0bf75bd2
Vivek Thakur <itsvivekthakr@gmial.com>
2025-05-12 15:13:09 +0545

    Introduction to Python

---
+++ 1.py
@@ -0,0 +1,13 @@
+# print anything: say - Hello World!
+print("Hello World!")
+
+
+# Python is a high-level programming language.
+# It supports platforms like Windows, Linux, Mac OS, etc.
+
+
+# Why Python?
+# Python is very easy to learn and use.
+# Pythin is versatile and can be used for various purposes like: Web Development, Data Science, Machine Learning, Artificial Intelligence, etc.
+
+
