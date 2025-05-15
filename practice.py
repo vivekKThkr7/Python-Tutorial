86bf6a5058048f4130fd326d28a1552e35e98e31
Vivek Thakur <itsvivekthakr@gmial.com>
2025-05-15 23:07:34 +0545

    basic Practice day3

---
+++ practice.py
@@ -0,0 +1,24 @@
+# practice python
+# 1. Write a Python program to find the sum of two numbers.
+num1 = 10
+num2 = 20
+# print("The sum of", + num1 + " and " + num2 + " is " + num1 +num2)
+print(f"The sum of {num1} and {num2} is {num1 + num2}")
+
+
+fname = "Vivek"
+lname = "Thakur"
+print("The full name is ", fname + " " + lname)
+
+# 2. Write a Python program to find the sum of two numbers with user input.
+num3 = int(input("Enter First Number: "))
+num4 = int(input("Enter second number: "))
+
+print(f"The sum of {num3} and {num4} is {num3 + num4}")
+
+# 3. write a python program using different data types
+name = "Vivek Kumar Thakur"
+age = 25
+isEligilbe = True
+skill = None
+print(f"The man {name}, is {age} years of old. He is eligile to vote {isEligilbe}. And he is unskilled {skill}")
